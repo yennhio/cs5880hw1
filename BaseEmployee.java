ce3e631b88dd6f06d117ffc452675763342f2be7
Yennhi Dang <nhidangn@gmail.com>
2023-03-07 11:45:17 -0800

    add setters & getters

--- BaseEmployee.java
+++ BaseEmployee.java
@@ -1,3 +1,12 @@
-public class BaseEmployee {
+public class BaseEmployee extends Employee {
     
+    int baseSalary;
+
+    public void setBaseSalary(int newBaseSalary) {
+        this.baseSalary = newBaseSalary;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
 }

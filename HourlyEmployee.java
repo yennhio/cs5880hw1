c9e03965cd26ce2b924eef991d7ac80e7d92bfc9
Yennhi Dang <nhidangn@gmail.com>
2023-03-07 11:08:57 -0800

    add setters and getters

--- HourlyEmployee.java
+++ HourlyEmployee.java
@@ -1,3 +1,22 @@
-public class HourlyEmployee {
+public class HourlyEmployee extends Employee {
+
+    int wage;
+    int hoursWorked;
+
+    public void setWage(int newWage) {
+        this.wage = newWage;
+    }
+
+    public void setHoursWorked(int newHoursWorked) {
+        this.hoursWorked = newHoursWorked;
+    }
+
+    public int getWage() {
+        return wage;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
     
 }

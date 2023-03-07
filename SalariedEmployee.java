7a97751ab94f05d3573c1e037ff816d577461a86
Yennhi Dang <nhidangn@gmail.com>
2023-03-07 10:46:35 -0800

    add setter and getter

--- SalariedEmployee.java
+++ SalariedEmployee.java
@@ -1,3 +1,13 @@
-public class SalariedEmployee {
+public class SalariedEmployee extends Employee {
     
+    int weeklySalary;
+
+    public void setWeeklySalary(int newWeeklySalary) {
+        this.weeklySalary = newWeeklySalary;
+    }
+
+    public int getWeeklySalary() {
+        return weeklySalary;
+    }
+
 }

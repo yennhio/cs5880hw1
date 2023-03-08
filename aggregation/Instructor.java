ff0a6a9843394b1e878c2cdddddaeb9c9179e33b
Yennhi Dang <nhidangn@gmail.com>
2023-03-08 15:08:10 -0800

    add getters and setters

--- aggregation/Instructor.java
+++ aggregation/Instructor.java
@@ -6,5 +6,29 @@ public class Instructor {
     private String lastName;
     private int officeNumber;
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setOfficeNumber(int officeNumber) {
+        this.officeNumber = officeNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getOfficeNumber() {
+        return officeNumber;
+    }
+
     
 }

8cbb07bb41de9acb2cd1eebc1a953f530756a613
Yennhi Dang <nhidangn@gmail.com>
2023-03-08 15:10:52 -0800

    add getters and setters

--- aggregation/Textbook.java
+++ aggregation/Textbook.java
@@ -5,5 +5,30 @@ public class Textbook {
     private String bookTitle;
     private String author;
     private String publisher;
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
     
 }
